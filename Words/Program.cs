ad4d774b8d5f7d6daa675610579b457d33e5dfdc
Dmitry <d.liapkin@gmail.com>
2022-06-09 17:39:43 +0300

    feat: added main game cycle

--- Words/Program.cs
+++ Words/Program.cs
@@ -13,11 +13,51 @@ namespace Words
 
         }
 
+        private static bool WordValidation(String sourceWord, String enteredWord)
+        {
+
+        }
+
         static void Main(string[] args)
         {
+            bool player1 = true, player2 = false, gameStatus = true;
+            List<String> usedWords = new List<String>();
             String sourceWord;
-            Console.WriteLine("Введите слово для начала игры.");
+            Console.WriteLine("Enter word to start the game.");
             sourceWord = Console.ReadLine();
+
+            while(gameStatus)
+            {
+                Console.WriteLine("Enter word:");
+                String enteredWord = Console.ReadLine();
+
+                if(!WordValidation(sourceWord,enteredWord))
+                {
+                    gameStatus = false;
+                    if(player1)
+                    {
+                        Console.WriteLine("Wrong word!\nPlayer 2 wins.");
+                    }
+                    if (player2)
+                    {
+                        Console.WriteLine("Wrong word!\nPlayer 1 wins.");
+                    }
+                }
+                else
+                {
+                    usedWords.Add(enteredWord);
+                    if (player1)
+                    {
+                        player1 = false;
+                        player2 = true;
+                    }
+                    if (player2)
+                    {
+                        player1 = true;
+                        player2 = false;
+                    }
+                }
+            }
         }
     }
 }
