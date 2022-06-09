837e087dbfee2be65bcbd2b4577176494faed233
Dmitry <d.liapkin@gmail.com>
2022-06-09 17:16:09 +0300

    Добавьте файлы проекта.

---
+++ Words/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Words
+{
+    class Program
+    {
+        protected Program()
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            String sourceWord;
+            Console.WriteLine("Введите слово для начала игры.");
+            sourceWord = Console.ReadLine();
+        }
+    }
+}
